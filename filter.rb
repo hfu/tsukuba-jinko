b6f0af5e3efba14040762261561d2c23086072a0
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2022-01-16 22:38:30 +0900

    update

--- filter.rb
+++ filter.rb
@@ -1,12 +1,14 @@
 require 'json'
 
-print "面積,人口\n"
+print "面積,人口,名称,コード\n"
 while gets
   f = JSON.parse($_)
   kigo_e = f['properties']['KIGO_E']
   next unless kigo_e.nil? or kigo_e == 'E1'
   area = f['properties']['AREA'] / 1000000.0
   population = f['properties']['JINKO']
-  print "#{area},#{population}\n"
+  s_name = f['properties']['S_NAME']
+  key_code = f['properties']['KEY_CODE']
+  print "#{area},#{population},#{s_name},#{key_code}\n"
 end
 
